bbe6084842b5d41c329a87bd7d17af6d33b1b274
suba466 <subashreethavaselvan@gmail.com>
2025-07-19 13:20:01 +0530

    Add files via upload

--- forwhiletask.js
+++ forwhiletask.js
@@ -1,18 +1,35 @@
 function checkPrime(){
-    let num= +document.getElementById("numberInput").value;
+     let number = Number(document.getElementById("numberInput").value);
     let isPrime=true;
-    for (let i=2;i<num;i++){
-        if(num%i==0){
+    for(let i=2;i<number;i++){
+        if(number<2){
+            isPrime=false;
+            break;
+        }
+        if(i<number && number%i==0){
             isPrime=false;
             break;
         }
     }
-    if(num<=1){
-        isPrime=false;
-    }
-    if(isPrime){
-        document.getElementById("result").innerHTML=num + "is a Prime Number"
-    }else{
-        document.getElementById("result").innerHTML=num + "is not a Prime Number"
-    }
+  if(isPrime){
+     document.getElementById("result").innerText = number + " is a Prime Number.";
+   }else{
+    document.getElementById("result").innerText=number + " is NOT a Prime Number."
+   }
 }
+function generateFibonacci(){
+      let n=Number(document.getElementById("inputNum").value);
+      let outputDiv=document.createElement("output");
+      outputDiv.innerHTML = " ";
+      let a=0,b=1;
+      for(let i=0;i<n;i++){
+            let span=document.createElement("span");
+            span.innerText=a+" ";
+            outputDiv.appendChild(span);
+            let next=a+b;
+            a=b;
+            b=next;
+      }
+      document.body.appendChild(outputDiv);
+}
+
