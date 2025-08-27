02e632d22876a833ce7cca2ced013876c8703705
suba466 <subashreethavaselvan@gmail.com>
2025-08-27 18:41:04 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousal from './Carousal.jsx'
 import App from './App.jsx';
 import Urban from './Urban.jsx';
-import Task from './Task.jsx'
+
 import Usecontext from './profilecontext.jsx';
 import Theme from './Theme.jsx';
 import User from './User.jsx';
