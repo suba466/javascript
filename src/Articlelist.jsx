7e85d9d5a23616007fbd73f153ebc9a45e167cc3
suba466 <subashreethavaselvan@gmail.com>
2025-09-14 19:25:32 +0530

    Add files via upload

--- src/Articlelist.jsx
+++ src/Articlelist.jsx
@@ -3,28 +3,12 @@ import { useLocation } from "react-router-dom";
 const Articlelist = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const category = queryParams.get("category");
-
-  const articles = [
-    { id: 1, title: "React Basics", category: "tech" },
-    { id: 2, title: "Healthy Living Tips", category: "health" },
-    { id: 3, title: "AI in 2025", category: "tech" },
-    { id: 4, title: "Yoga for Beginners", category: "health" },
-    { id: 5, title: "Cybersecurity Trends", category: "tech" }
-  ];
-
-  const filteredArticles = category
-    ? articles.filter(article => article.category === category)
-    : articles;
-
+  const name = queryParams.get("name") || "Guest";
+  const age=queryParams.get("age") || "unknown";
   return (
     <div style={{ textAlign: "center" }}>
-      <h2>Articles {category && `in ${category}`}</h2>
-      <ul>
-        {filteredArticles.map(article => (
-          <li key={article.id}>{article.title}</li>
-        ))}
-      </ul>
+      <h2>Profile Page</h2>
+     <p> Hello <strong>{name}</strong>! you are <strong>{age}</strong> years young!!</p>
     </div>
   );
 };
--- src/Articles.jsx
+++ src/Articles.jsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link} from "react-router-dom";
 import Articlelist from './Articlelist.jsx';
 
-const Articles = () => {
-  return (
-    <Routes>
-      <Route path="/articlelist" element={<Articlelist />} />
-    </Routes>
-  );
+const Articles=()=>{
+  return(
+    <div style={{textAlign:"center", marginTop:"2rem"}}>
+      <h1>Welcome to Demo App</h1>
+      <nav style={{marginBottom:"1 rem"}}>
+        <Link to="/articlelist?name=Suba&age=22" style={{marginRight:"1 rem"}}>
+        View Suba's Profile</Link>
+        <Link to="//articlelist?name=Buddy&age=1000">View Buddy's Profile</Link>
+      </nav>
+      <Routes>
+        <Route path="/articlelist" element={<Articlelist/>}></Route>
+      </Routes>
+    </div>
+  )
 };
-
 export default Articles;
